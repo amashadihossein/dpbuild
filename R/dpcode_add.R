fcf863ece7bdecf21349112de15853f4034b1088
camorosi <claraamorosi@gmail.com>
2022-09-28 17:04:18 -0700

    Add targets-specific journal, update dpcode_add for targets

--- R/dpcode_add.R
+++ R/dpcode_add.R
@@ -17,10 +17,6 @@ dpcode_add <- function(project_path, use_targets=F){
   if(!fs::dir_exists(fs::path_tidy(glue::glue("{project_path}/R"))))
     fs::dir_create(fs::path_tidy(glue::glue("{project_path}/R")))
 
-  flname_dpjournal <- flname_xos_get(fl = "dp_journal.RMD")
-  fs::file_copy(path = system.file(flname_dpjournal, package = "dpbuild"),
-                new_path = project_path)
-  
   if (!use_targets) {
     fs::file_copy(path = system.file("dp_make.R", package = "dpbuild"),
       new_path = project_path)
@@ -32,35 +28,51 @@ dpcode_add <- function(project_path, use_targets=F){
       new_path = glue::glue("{project_path}/R/global.R"))
     renv::install(packages = "drake",prompt = F)
 
+    # drake specific journal
+    flname_dpjournal <- flname_xos_get(fl = "dp_journal.RMD")
+    fs::file_copy(path = system.file(flname_dpjournal, package = "dpbuild"),
+                  new_path = project_path)
+
   } else {
     fs::file_copy(path = system.file("_targets.R", package = "dpbuild"),
       new_path = project_path)
     fs::file_move(path = glue::glue("{project_path}/_targets.R"),
       new_path = glue::glue("{project_path}/dp_make.R"))
 
+    # targets specific journal (renamed)
+    flname_dpjournal <- flname_xos_get(fl = "dp_journal_targets.RMD")
+    fs::file_copy(path = system.file(flname_dpjournal, package = "dpbuild"),
+                  new_path = project_path)
+    fs::file_move(path = glue::glue("{project_path}/dp_journal_targets.RMD"),
+                  new_path = glue::glue("{project_path}/dp_journal.RMD"))
+
     fs::file_copy(path = system.file("global_targets.R", package = "dpbuild"),
       new_path = glue::glue("{project_path}/R"))
     fs::file_delete(path =  glue::glue("{project_path}/R/global.R"))
     fs::file_move(path = glue::glue("{project_path}/R/global_targets.R"),
       new_path = glue::glue("{project_path}/R/global.R"))
-    renv::install(packages = "targets",prompt = F)
+    renv::install(packages = c("drake", "tarchetypes"), prompt = F)
   }
 
-  # pkgs_dependencies <- renv::dependencies(path = project_path, 
+  # pkgs_dependencies <- renv::dependencies(path = project_path,
   #                                         root = project_path) %>%
   #   dplyr::pull(Package)
-  
+
   # renv::install(packages = pkgs_dependencies,prompt = F)
-  
+
   # snapshot with pkgs in global
-  renv::snapshot(prompt = F) #TODO: look into  explicitly adding pkgs 
-  
+  renv::snapshot(prompt = F) #TODO: look into  explicitly adding pkgs
+
   # commit
   repo <- git2r::repository(path = project_path)
-  git2r::add(repo = repo,path =
+  git2r::add(repo = repo, path =
                fs::path_tidy(glue::glue("{project_path}/dp_make.R")))
-  git2r::add(repo = repo,path =
+  git2r::add(repo = repo, path =
                fs::path_tidy(glue::glue("{project_path}/{flname_dpjournal}")))
+  git2r::add(repo = repo, path =
+               fs::path_tidy(glue::glue("{project_path}/R/global.R")))
+  git2r::add(repo = repo, path =
+               fs::path_tidy(glue::glue("{project_path}/renv.lock")))
   repo <- git2r::commit(repo = repo,
                         message = "Added template code to dp project")
   return(repo)
