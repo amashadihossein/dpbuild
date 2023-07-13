4912966eeb67e7fe35e3afc3f45a24f6576e04b6
geauxdojang <energeticsom@gmail.com>
2023-07-12 17:04:12 -0700

    remove full option from pin_versions

--- R/utils.R
+++ R/utils.R
@@ -57,8 +57,7 @@ get_pin_version <- function(d, pin_name, pin_description) {
 
   pin_version <- pins::pin_versions(
     name = pin_name,
-    board = local_board_folder,
-    full = F
+    board = local_board_folder
   ) %>% dplyr::pull(.data$version)
   pins::pin_delete(names = pin_name, board = local_board_folder)
 
