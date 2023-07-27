06e556ce05e2995ccbe1232053b5bf8a02ef7543
camorosi <50029049+camorosi@users.noreply.github.com>
2023-07-27 12:59:54 -0700

    Bump version, remove labkey, add news, fix gha dependencies

--- tests/testthat/test-fn_dry.R
+++ tests/testthat/test-fn_dry.R
@@ -15,19 +15,19 @@ testthat::test_that("This would expect the equality in fn_dry function", {
   )
 })
 
-testthat::test_that("This would expect the error with creds_set_labkey in fn_dry function", {
-  testthat::expect_error(
-    fn_dry(
-      creds_set_labkey(api_key = "OPIKJDI5LPKnjdBKsGES")
-    )
-  )
-})
+# testthat::test_that("This would expect the error with creds_set_labkey in fn_dry function", {
+#   testthat::expect_error(
+#     fn_dry(
+#       creds_set_labkey(api_key = "OPIKJDI5LPKnjdBKsGES")
+#     )
+#   )
+# })
 
-testthat::test_that("This would expect the equality in fn_dry function", {
-  testthat::expect_equal(
-    fn_dry(
-      creds_set_labkey(api_key = Sys.getenv("API_KEY"))
-    ),
-    "creds_set_labkey(api_key = Sys.getenv(\"API_KEY\"))"
-  )
-})
+# testthat::test_that("This would expect the equality in fn_dry function", {
+#   testthat::expect_equal(
+#     fn_dry(
+#       creds_set_labkey(api_key = Sys.getenv("API_KEY"))
+#     ),
+#     "creds_set_labkey(api_key = Sys.getenv(\"API_KEY\"))"
+#   )
+# })
