770a1959764a39b7c725c419c37f6552e95cdad7
kutlub1 <burak.kutlu@bms.com>
2025-10-27 13:27:49 -0700

    Refactor dp_write function for consistent serialization and hash calculation

--- R/dp_write.R
+++ R/dp_write.R
@@ -21,8 +21,10 @@ dp_write <- function(data_object, type = 'rds', project_path = ".") {
       "a properly formatted dp"
     )))
   }
-
+  
   dataobj_path <- save_object(data_object, project_path = project_path, type = type)
+
+  data_object <- object_read( project_path = project_path, type = type)
 
   log_note <- dplognote_get(
     data_object = data_object,
